19ab49a51be05139562b9fbcc5ba8d4bf3d99b73
Dana Avramescu <avramescudana@protonmail.com>
2025-05-13 15:11:52 +0300

    Code cleanup, moved relevant functions to shape.jl

--- src/shape.jl
+++ src/shape.jl
@@ -1,4 +1,5 @@
-using SpecialFunctions
+using SpecialFunctions # For Bessel functions
+using Roots  # For root-finding
 
 """
 Gaussian radial envelope: exp(-alpha * r^2).
@@ -8,27 +9,49 @@ function radial_envelope(r, alpha=0.05)
 end
 
 """
-Compute the angular modulation as a weighted sum of e^(im*phi) * J_n(kr).
+Compute the n-th positive root of the Bessel function J_m.
 """
-function angular_modulation(phi, r, coeff_dict, k=1.0)
-    modulation = zeros(Float64, size(phi))
-    for ((n, m), amp) in coeff_dict
-        modulation .+= amp .* real(exp(im * m .* phi) .* besselj(n, k .* r))
+function besseljzero(m, n; tol=1e-8, max_iter=100)
+    f(x) = besselj(m, x) 
+    roots = []
+    x_start = 0.1  
+    step_size = 0.5 
+    while length(roots) < n
+        x_end = x_start + step_size  
+        if f(x_start) * f(x_end) < 0  
+            root = find_zero(f, (x_start, x_end), Bisection(), tol=tol)
+            push!(roots, root)
+            x_start = root + tol  
+        else
+            x_start = x_end  
+        end
     end
-    return modulation
+    return roots[n]  
+end
+
+"""
+Compute the weighted sum of e^(im*phi) * J_m(α_{mn}r/a).
+"""
+function circular_membrane(phi, r, coeff_dict, a=1.0)
+    modulation = ones(size(phi))
+    for ((m, n), amp) in coeff_dict
+        α_mn = besseljzero(m, n)  
+        modulation .+= amp .* real.(exp.(im * m .* phi) .* besselj.(m, α_mn ./ a .* r))
+    end
+    return modulation  
 end
 
 """
 Build the base density field as:
-   density_base = envelope(r) * exp(angular_modulation(phi, r)).
+   density_base = envelope(r) * circular_membrane(phi, r).
 """
-function build_base_density_2D_general(X, Y, coeff_dict; alpha=0.05, envelope_func=radial_envelope, k=1.0)
+function density_2D(X, Y, coeff_dict; alpha=0.05, envelope_func=radial_envelope, a=1.0)
     r = sqrt.(X.^2 .+ Y.^2)
     phi = atan.(Y, X)
     phi = ifelse.(phi .< 0, phi .+ 2π, phi)
 
     env = envelope_func(r, alpha)
-    ang_mod = angular_modulation(phi, r, coeff_dict, k)
-    density_base = env .* exp.(ang_mod)
+    memb = circular_membrane(phi, r, coeff_dict, a)
+    density_base = env .* memb
     return density_base
 end
